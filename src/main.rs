990f6b072df8ae96de23afc00aa60e48f290ab49
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-11-28 03:01:25 +0530

    fix: added comments

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn main() {
 
     // Game Logic
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
+    // Loops are a way to loop indefinitely over instructions unless if break; is used to break out of the loop.
     loop {
         //  Here we ask the user for an input
         println!("Please input your guess.");
@@ -24,15 +24,29 @@ fn main() {
          * We do this by giving it a keyword 'mut'.
          */
         let mut guess = String::new();
+
+        // Reading for the user input.
+        // Note: Look how Rust forces us to except for an error since read_line returns a result type and a enum with two variants.
+        // Ok(T) -> Which means, everything is good and returns the T Type value.
+        // Err(E) -> Something went wrong and the program has crashed with the error E. This error needs to be managed.
         io::stdin()
             .read_line(&mut guess)
             .expect("There was an error while reading the input.");
 
+        /*
+         * Now we are converting the string input to a unsigned 32 bit integer. The same type as our secret number.
+         * This also returns a result as described above. The error needs to be handled properly for program to exit out.
+         */
         let guess: u32 = guess
             .trim()
             .parse()
             .expect("There was a an error parsing the input to u32.");
 
+        /*
+         * Rust has pattern matching. It is nothing but a, "Hey look this can variable can be like this AND this." (notice AND).
+         * Every case needs to be managed explicitly or by using wildcards `_`.
+         * Here we use break; to break out of the loop. This is important otherwise it'll run indefinitely.
+         */
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too Small!"),
             Ordering::Equal => {
